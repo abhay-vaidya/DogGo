f4931c16b88b5c5bddce82242d56e7dc390c30b5
ousmane.amadou <ousmane.amadou89@gmail.com>
2017-01-07 21:05:12 -0500

    Updated Database Code

--- backend/flsk.py
+++ backend/flsk.py
@@ -1,4 +1,10 @@
 from flask import Flask
+import pypyodbc
+
+connection = pypyodbc.connect('Driver={SQL Server};',
+        'Server=tcp:utscdogs.database.windows.net,1433', 'Database=dog_base;', 'Uid=utsc2017;Pwd=Blackychan313;')
+
+cursor = 
 app = Flask(__name__)
 
 @app.route('/', )
