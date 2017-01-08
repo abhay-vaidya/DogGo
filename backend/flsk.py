e5d2c5b00e4c1fd512aea7e3ff06edf297f07a3b
ousmane.amadou <ousmane.amadou89@gmail.com>
2017-01-08 00:26:23 -0500

    Database Changes

--- backend/flsk.py
+++ backend/flsk.py
@@ -5,11 +5,12 @@ import pypyodbc
 import urllib
 
 conDEBUG = "DRIVER={SQL Server};Database=dog_base;SERVER=tcp:utscdogs.database.windows.net,1433';Uid=utsc2017;Pwd=Blackychan313;"
-conDEBUG = urllib.parse.quote_plus(conDEBUG)
+conDEBUG = urllib.quote_plus(conDEBUG)
 conDEBUG = "mssql+pyodbc:///?odbc_connect=%s" % conDEBUG
 
-app.config['SQLALCHEMY_DATABASE_URI'] = conDEBUG
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = conDEBUG
 db = SQLAlchemy(app)
 
 class Example(db.Model):
@@ -17,7 +18,7 @@ class Example(db.Model):
     id = db.Column('id',db.Integer,primary_key=True)
     data = db.Column('data', db.Unicode)
 
-    
+
 @app.route('/')
 def index():
     return 'Index Page'
